4cb4088a83ad9b3d69247bd60919f004d6985ec8
Rustam <rustamergashev.sp@gmail.com>
2024-10-01 15:21:07 +0500

    MINOR: Added spatie/laravel-permissions

--- routes/web.php
+++ routes/web.php
@@ -23,40 +23,6 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-/**
- * TODO Dashboard:
- *  1. Add link to home page [4]
- *  2. Add icons to menu items [1]
- *  3. Make fields sortable: [5]
- *      - price, rooms, gender, status, branch
- *  4. Hide address and description from AdResource index [2]
- *  5. Display uploaded image via relationship (show only related images count) [100]
- *  6. Display ads attached to branches [100]
- *  7. Display ads attached to users [100]
- *  8. Translate + Create button on index pages []
- *  9. Add Bookmarked ads resource and all related stuff
- *
- * TODO home page:
- *  1. Add icons
- *  2. Show corresponding data on index and detail pages (branch, gender, rooms)
- *  3. Implement bookmarking feature (use many to many relationship)
- */
-
-
-
-
-/**
- * TODO:
- *  1. Make one code base
- *   - Fork from the latest version (all branches)
- *   - Clone the repo
- *   - Combine all existing features: code, db
- *   - Pull request
- */
-
-
-
-
 
 
 
